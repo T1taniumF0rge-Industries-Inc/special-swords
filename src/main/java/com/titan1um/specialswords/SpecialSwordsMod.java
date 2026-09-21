66b50a56eb49f77a4ce59e8aa4efd2980867bdc6
Titan1um <gamersoftware.corp@gmail.com>
2026-09-21 20:47:32 +0200

    Reformat code and fix server damage callback

--- src/main/java/com/titan1um/specialswords/SpecialSwordsMod.java
+++ src/main/java/com/titan1um/specialswords/SpecialSwordsMod.java
@@ -1,7 +1,23 @@
 package com.titan1um.specialswords;
+
 import net.fabricmc.api.ModInitializer;
-public class SpecialSwordsMod implements ModInitializer{
- @Override public void onInitialize(){SpecialSwordManager.register();}
- public static int getSpecialSwordRenameCost(String name){return switch(SwordUtils.normalizeSpecialSwordName(name)){case SwordUtils.LIGHTNING_SWORD->30;case SwordUtils.LIFESTEAL_SWORD,SwordUtils.DASH_SWORD->35;default->0;};}
- public static String normalizeSpecialSwordName(String name){return SwordUtils.normalizeSpecialSwordName(name);}
+
+public class SpecialSwordsMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        SpecialSwordManager.register();
+    }
+
+    public static int getSpecialSwordRenameCost(String name) {
+        return switch (SwordUtils.normalizeSpecialSwordName(name)) {
+            case SwordUtils.LIGHTNING_SWORD -> 30;
+            case SwordUtils.LIFESTEAL_SWORD, SwordUtils.DASH_SWORD -> 35;
+            default -> 0;
+        };
+    }
+
+    public static String normalizeSpecialSwordName(String name) {
+        return SwordUtils.normalizeSpecialSwordName(name);
+    }
 }
